7158975cee8257ab4d61ae38d3c589dce59319a2
rubibyte <rubibyte@gmail.com>
2021-11-12 00:37:05 +0800

    add charity list

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,18 @@
-<h1>AULIA</h1>
+<script>
+    import { CharityList } from "./CharityList.svelte";
+
+    let name = "Aulia";
+    setTimeout(() => {
+        name = "Rahmat"
+    }, 2000);
+</script>
+
+<h1>{name}</h1>
+<CharityList />
+
+<style>
+    h1{
+        font-size: 3em;
+        color: yellowgreen;
+    }
+</style>
